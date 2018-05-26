3f1e21e40b9131af9730dcc0c0866d1ae5ea87f2
misirlou-tg <39652424+misirlou-tg@users.noreply.github.com>
2018-05-25 21:06:51 -0500

    Interim commit trying to use a copy of the shared MainPage.xaml. Encountering errors with the generated files.

--- Samples/UserInfo/cppwinrt/MainPage.h
+++ Samples/UserInfo/cppwinrt/MainPage.h
@@ -6,20 +6,17 @@
 
 #include "MainPage.g.h"
 
-namespace winrt::UserInfoCppWinRt::implementation
+namespace winrt::SDKTemplate::implementation
 {
     struct MainPage : MainPageT<MainPage>
     {
         MainPage();
 
-        int32_t MyProperty();
-        void MyProperty(int32_t value);
-
-        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        //void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
     };
 }
 
-namespace winrt::UserInfoCppWinRt::factory_implementation
+namespace winrt::SDKTemplate::factory_implementation
 {
     struct MainPage : MainPageT<MainPage, implementation::MainPage>
     {
