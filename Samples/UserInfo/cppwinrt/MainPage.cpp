03e6c4b8c608ac48d23227d30d4086ede1a64b9f
misirlou-tg <39652424+misirlou-tg@users.noreply.github.com>
2018-05-25 23:48:04 -0500

    Adding just enough to MainPage to get it to compile and set the title.

--- Samples/UserInfo/cppwinrt/MainPage.cpp
+++ Samples/UserInfo/cppwinrt/MainPage.cpp
@@ -4,15 +4,35 @@
 using namespace winrt;
 using namespace Windows::UI::Xaml;
 
+// TODO: Would like this to be in the SDKTemplate namespace
+//       (the issue was that the generated files were in SDKTemplate sub-dir)
+//       (could solve by putting that in the include path?)
+
 namespace winrt::UserInfoCppWinRt::implementation
 {
+    // TODO: In the C++/CX version this is supplied as a property of a partial class
+    //       (would that go in our IDL?)
+    const wchar_t FEATURE_NAME[] = L"User info C++/WinRT sample";
+
     MainPage::MainPage()
     {
         InitializeComponent();
+        SampleTitle().Text(FEATURE_NAME);
     }
 
-    //void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
-    //{
-    //    myButton().Content(box_value(L"Clicked"));
-    //}
+    void MainPage::Button_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+    }
+
+    void MainPage::ScenarioControl_SelectionChanged(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::Controls::SelectionChangedEventArgs const& args)
+    {
+    }
+
+    void MainPage::Footer_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args)
+    {
+    }
+
+    void MainPage::OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs const& args) const
+    {
+    }
 }
