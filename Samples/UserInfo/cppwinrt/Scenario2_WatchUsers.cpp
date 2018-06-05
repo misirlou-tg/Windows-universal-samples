abe08ddc1dcb5d971381d71f42bcff23ac9950d1
misirlou-tg <39652424+misirlou-tg@users.noreply.github.com>
2018-06-04 22:32:22 -0500

    Implementing Scenario2 except OnUser* handlers
    All of StartWatching() & StopWatching() are implemented.
    OnEnumerationCompleted() and OnWatcherStopped() are implemented.
    The OnUserAdded/Removed/Updated handlers are not.

--- Samples/UserInfo/cppwinrt/Scenario2_WatchUsers.cpp
+++ Samples/UserInfo/cppwinrt/Scenario2_WatchUsers.cpp
@@ -1,31 +1,143 @@
 ﻿#include "pch.h"
 #include "Scenario2_WatchUsers.h"
 
+#include "MainPage.h"
+#include "single_threaded_observable_vector.h"
+
 using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::System;
 using namespace Windows::UI::Xaml;
 
 namespace winrt::SDKTemplate::implementation
 {
     Scenario2_WatchUsers::Scenario2_WatchUsers()
+        : userWatcher(nullptr)
     {
         InitializeComponent();
+
+        models = winrt::make<single_threaded_observable_vector<IInspectable>>();
+        UserList().DataContext(models);
     }
 
-    void Scenario2_WatchUsers::OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs const& args)
+    void Scenario2_WatchUsers::OnNavigatedFrom(Windows::UI::Xaml::Navigation::NavigationEventArgs const& args)
     {
         UNREFERENCED_PARAMETER(args);
+
+        StopWatching();
     }
 
     void Scenario2_WatchUsers::StartWatching()
     {
+        if (auto rootPage = MainPage::Current())
+        {
+            rootPage.NotifyUser(L"", NotifyType::StatusMessage);
+        }
+
+        nextUserNumber = 1;
+        models.Clear();
+        userWatcher = User::CreateWatcher();
+        userAddedToken = userWatcher.Added({ this, &Scenario2_WatchUsers::OnUserAdded });
+        userUpdatedToken = userWatcher.Updated({ this, &Scenario2_WatchUsers::OnUserUpdated });
+        userRemovedToken = userWatcher.Removed({ this, &Scenario2_WatchUsers::OnUserRemoved });
+        enumerationCompletedToken = userWatcher.EnumerationCompleted({ this, &Scenario2_WatchUsers::OnEnumerationCompleted });
+        watcherStoppedToken = userWatcher.Stopped({ this, &Scenario2_WatchUsers::OnWatcherStopped });
+        userWatcher.Start();
+        StartButton().IsEnabled(false);
+        StopButton().IsEnabled(true);
     }
 
     void Scenario2_WatchUsers::StopWatching()
     {
+        if (userWatcher != nullptr)
+        {
+            // Unregister all event handlers in case events are in flight.
+            userWatcher.Added(userAddedToken);
+            userWatcher.Updated(userUpdatedToken);
+            userWatcher.Removed(userRemovedToken);
+            userWatcher.EnumerationCompleted(enumerationCompletedToken);
+            userWatcher.Stopped(watcherStoppedToken);
+            userWatcher.Stop();
+            userWatcher = nullptr;
+            StartButton().IsEnabled(true);
+            StopButton().IsEnabled(false);
+
+            // The C++/CX version didn't clear the status, but why not?
+            if (auto rootPage = MainPage::Current())
+            {
+                rootPage.NotifyUser(L"", NotifyType::StatusMessage);
+            }
+        }
     }
 
     Windows::Foundation::Collections::IVector<IInspectable> Scenario2_WatchUsers::Users()
     {
-        return winrt::single_threaded_vector<IInspectable>();
+        return models;
+    }
+
+    void Scenario2_WatchUsers::OnUserAdded(UserWatcher sender, UserChangedEventArgs e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        ::OutputDebugStringW(L"OnUserAdded() called\n");
+    }
+
+    void Scenario2_WatchUsers::OnUserUpdated(UserWatcher sender, UserChangedEventArgs e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        ::OutputDebugStringW(L"OnUserUpdated() called\n");
+    }
+
+    void Scenario2_WatchUsers::OnUserRemoved(UserWatcher sender, UserChangedEventArgs e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        ::OutputDebugStringW(L"OnUserRemoved() called\n");
+    }
+
+    // **************************************************
+    // TODO: Any way to make OnEnumerationComplete async?
+    // **************************************************
+    static IAsyncAction EnumerationCompletedAsync(SDKTemplate::MainPage rootPage)
+    {
+        // UI work must happen on the UI thread.
+        co_await winrt::resume_foreground(rootPage.Dispatcher());
+        rootPage.NotifyUser(L"Enumeration complete. Watching for changes...", NotifyType::StatusMessage);
+    }
+
+    void Scenario2_WatchUsers::OnEnumerationCompleted(Windows::System::UserWatcher sender, IInspectable e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        if (auto rootPage = MainPage::Current())
+        {
+            EnumerationCompletedAsync(rootPage);
+        }
+    }
+
+    // *********************************************
+    // TODO: Any way to make OnWatcherStopped async?
+    // *********************************************
+    static IAsyncAction WatcherStoppedAsync(SDKTemplate::MainPage rootPage, implementation::Scenario2_WatchUsers *pThis)
+    {
+        // UI work must happen on the UI thread.
+        co_await winrt::resume_foreground(rootPage.Dispatcher());
+        pThis->StopWatching();
+    }
+
+    void Scenario2_WatchUsers::OnWatcherStopped(Windows::System::UserWatcher sender, IInspectable e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        if (auto rootPage = MainPage::Current())
+        {
+            WatcherStoppedAsync(rootPage, this);
+        }
     }
 }
--- Samples/UserInfo/cppwinrt/Scenario2_WatchUsers.h
+++ Samples/UserInfo/cppwinrt/Scenario2_WatchUsers.h
@@ -12,12 +12,29 @@ namespace winrt::SDKTemplate::implementation
     {
         Scenario2_WatchUsers();
 
-        virtual void OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs const& args);
+        virtual void OnNavigatedFrom(Windows::UI::Xaml::Navigation::NavigationEventArgs const& args);
 
         void StartWatching();
         void StopWatching();
 
         Windows::Foundation::Collections::IVector<IInspectable> Users();
+
+    private:
+        Windows::Foundation::Collections::IVector<IInspectable> models;
+        int nextUserNumber;
+        Windows::System::UserWatcher userWatcher;
+
+        winrt::event_token userAddedToken;
+        winrt::event_token userUpdatedToken;
+        winrt::event_token userRemovedToken;
+        winrt::event_token enumerationCompletedToken;
+        winrt::event_token watcherStoppedToken;
+
+        void OnUserAdded(Windows::System::UserWatcher sender, Windows::System::UserChangedEventArgs e);
+        void OnUserUpdated(Windows::System::UserWatcher sender, Windows::System::UserChangedEventArgs e);
+        void OnUserRemoved(Windows::System::UserWatcher sender, Windows::System::UserChangedEventArgs e);
+        void OnEnumerationCompleted(Windows::System::UserWatcher sender, IInspectable e);
+        void OnWatcherStopped(Windows::System::UserWatcher sender, IInspectable e);
     };
 }
 
