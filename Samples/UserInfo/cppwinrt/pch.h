d77c620c0950dfbe6716d3b397d3d1ab32d68321
misirlou-tg <39652424+misirlou-tg@users.noreply.github.com>
2018-10-13 17:35:19 -0500

    Switching UserInfoCppWinRt to Win 10 1809
    Switching this project to use the Windows Platform SDK 10.0.17763.0 which is Win 10 1809. Leaving the min version at 10.0.17134.0 which is Win 10 1803.
    
    The new SDK required a couple of project build changes and including more headers in the pre-compiled header file.

--- Samples/UserInfo/cppwinrt/pch.h
+++ Samples/UserInfo/cppwinrt/pch.h
@@ -5,8 +5,10 @@
 
 #pragma once
 
-#define NOMINMAX
-
+#include <windows.h>
+#include <unknwn.h>
+#include <restrictederrorinfo.h>
+#include <hstring.h>
 #include "winrt/Windows.Foundation.h"
 #include "winrt/Windows.ApplicationModel.Activation.h"
 #include "winrt/Windows.System.h"
