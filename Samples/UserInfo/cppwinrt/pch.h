a68bbb8bc3b7aafe6a4c651f5b48adee5ddb782e
misirlou-tg <39652424+misirlou-tg@users.noreply.github.com>
2020-06-13 09:55:35 -0500

    Getting C++/WinRT project to compile with latest tools
    Not a part of this commit, but I first switched to Version 2 of the C++/WinRT VS Extension.
    
    Adding NuGet package that is required for Version 2 of the C++/WinRT extension.
    
    Adding a #import to pch.h, apparently due to more restructuring of C++/WinRT headers, etc. (I think they now pull less in to speed up the build, now you have to be more specific about what you really need).
    
    Recreating the self-signed signing cert. I got an error from the build "Certificate does not match supplied signing thumbprint: ...". Short story: The cert was somehow "bad". Long story: I followed some directions on the internet that said to view the cert in the app manifest editor. When I did this I got an error when it tried to load the cert (I clicked "Select from file..." in the instructions that follow). To get to that screen where you view/create the cert: Double click on Package.appxmanifest in the solution (open the 'designer for it'), click on the Packaging tab, click the "Choose Certificate..." button, on the popup dialog you can click "Select from file..." or "Create...".
    https://www.rudyhuyn.com/blog/2019/08/05/how-to-fix-certificate-does-not-match-supplied-signing-thumbprint/
    
    Switching Windows SDK Version to "Windows SDK 10.0 (latest installed version)", notice the lack of a build number.
    
    Switching to VS 2019 C++ compiler (14.2 platform toolset).
    
    Note on the changes to SDK & C++ compiler: Without these changes I was getting linker errors for imports like _WINRT_IMPL_LoadLibraryW (don't have the exact text). Google showed nothing. On some C++/WinRT pages about diagnosing compile/link issues I saw where they said, basically, use the latest tools. I switched both of these and the errors went away.

--- Samples/UserInfo/cppwinrt/pch.h
+++ Samples/UserInfo/cppwinrt/pch.h
@@ -10,6 +10,7 @@
 #include <restrictederrorinfo.h>
 #include <hstring.h>
 #include "winrt/Windows.Foundation.h"
+#include "winrt/Windows.Foundation.Collections.h"
 #include "winrt/Windows.ApplicationModel.Activation.h"
 #include "winrt/Windows.System.h"
 #include "winrt/Windows.Storage.Streams.h"
